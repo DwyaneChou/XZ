5527e9f92de12c1cd397b3f4e13c3feb14c15547
Zhou Lilong <dwyanechou@qq.com>
2021-01-26 16:01:34 +0800

    Update timer

--- src/xz.F90
+++ src/xz.F90
@@ -23,17 +23,20 @@
       
       real(r_kind), parameter :: timer_coef = 1.e9
       
-      integer(i4) :: timeStart,timeEnd
+      character(8 ) :: date_timer
+      character(10) :: timeStart,timeEnd
       
       character(20) :: procedure_name
-      integer  (i8) :: temporal_timer1,temporal_timer2
-      integer  (i8) :: nan_finder_timer1,nan_finder_timer2
-      integer  (i8) :: io_timer1,io_timer2
+      character(10) :: temporal_timer1,temporal_timer2
+      character(10) :: nan_finder_timer1,nan_finder_timer2
+      character(10) :: io_timer1,io_timer2
+      
+      real(r_kind) :: time1, time2
       
       integer :: output_interval
       
       !Timing start
-      call SYSTEM_CLOCK(timeStart)
+      call DATE_AND_TIME(date_timer,timeStart)
       
       call initParameters
       call init_reconstruction
@@ -47,9 +50,9 @@
       total_output_num = nsteps * dt / history_interval
       output_interval  = nint( history_interval / dt )
       
-      if( abs( history_interval / dt - int(history_interval / dt) ) > 1.e-14 )then
+      if( abs( history_interval / dt - output_interval ) > tolerance )then
         print*,'history_interval divides dt must be integer'
-        print*,abs( history_interval / dt - int(history_interval / dt) )
+        print*,abs( history_interval / dt - output_interval )
         stop
       endif
       
@@ -60,7 +63,7 @@
       total_mass0 = sum(stat(old)%q(1,ids:ide,kds:kde))
       
       do it = 1,nsteps
-        call system_clock(temporal_timer1)
+        call DATE_AND_TIME(date_timer,temporal_timer1)
         if(trim(adjustl(integral_scheme)) == 'RK3_TVD')then
           call RK3_TVD(stat(new),stat(old))
         elseif(trim(adjustl(integral_scheme)) == 'RK4')then
@@ -72,15 +75,15 @@
         elseif(trim(adjustl(integral_scheme)) == 'SSPRK')then
           call SSPRK(stat(new),stat(old))
         endif
-        call system_clock(temporal_timer2)
+        call DATE_AND_TIME(date_timer,temporal_timer2)
         
-        call system_clock(nan_finder_timer1)
+        call DATE_AND_TIME(date_timer,nan_finder_timer1)
         if(any(isnan(stat(new)%q(1,ids:ide,kds:kde))))then
           print*,'Nan appears at i k: ',maxloc(stat(new)%q(1,ids:ide,kds:kde),isnan(stat(new)%q(1,ids:ide,kds:kde)))
           print*,'after ',it,' steps'
           stop 'Model blow up, maybe smaller dt would help'
         endif
-        call system_clock(nan_finder_timer2)
+        call DATE_AND_TIME(date_timer,nan_finder_timer2)
         
         if( mod( it, output_interval )==0 .and. ( it >= output_interval ) )then
           total_mass = sum(stat(new)%q(1,ids:ide,kds:kde))!calc_total_mass     (stat(new))
@@ -90,9 +93,9 @@
           print*,'MCR = ',MCR
             
           output_idx = output_idx + 1
-          call system_clock(io_timer1)
+          call DATE_AND_TIME(date_timer,io_timer1)
           call history_write_stat(stat(new),output_idx)
-          call system_clock(io_timer2)
+          call DATE_AND_TIME(date_timer,io_timer2)
           print*,'output index/total',output_idx-1,'/',total_output_num
           procedure_name = 'IO'
           call procedure_timer(procedure_name,io_timer1,io_timer2,it)
@@ -108,18 +111,25 @@
       enddo
       
       ! Timing end
-      call SYSTEM_CLOCK(timeEnd)
+      call DATE_AND_TIME(date_timer,timeEnd)
       
-      print*,'It took ',real(timeEnd-timeStart,r_kind)/timer_coef,' seconds to run this program'
+      read(timeStart,*)time1
+      read(timeEnd  ,*)time2
+      print*,'It took ',time2-time1,' seconds to run this program'
       
     contains
       subroutine procedure_timer(procedure_name,t1,t2,step_num)
         character(20) :: procedure_name
-        integer  (i8) :: t1
-        integer  (i8) :: t2
+        character(10) :: t1
+        character(10) :: t2
         integer  (i4) :: step_num
         
-        print*,procedure_name//' elapse time', real( t2 - t1, r_kind ) / timer_coef, 'on step ', step_num
+        real(r_kind) :: time1, time2
+        
+        read(t1,*)time1
+        read(t2,*)time2
+      
+        print*,procedure_name//' elapse time', time2 - time1, 'on step ', step_num
         
       end subroutine procedure_timer
     
